0d09b4af191e81b95842fe448b2480800eb3fb91
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-09-02 17:49:49 +0300

    Реализовано создание БД и таблиц

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from src.api import HH
+from  src.db_creater import DBCreater
 import json
+from src.config import config
 
-vacancies = HH.load_vacancies("740")
-employer = HH.load_employer_data("740")
+# vacancies = HH.load_vacancies("740")
+# employer = HH.load_employer_data("740")
+#
+# print(json.dumps(vacancies, indent=4, ensure_ascii=False))
+# print(json.dumps(employer, indent=4, ensure_ascii=False))
+# print(len(vacancies))
 
-print(json.dumps(vacancies, indent=4, ensure_ascii=False))
-print(json.dumps(employer, indent=4, ensure_ascii=False))
-print(len(vacancies))
+params = config()
+
+db = DBCreater("hh_parser", params)
+db.create_database()
+db.create_tables()
